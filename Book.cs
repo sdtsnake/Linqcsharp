0296737956997d999bb49310006ba9552dd1fc6b
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2023-02-21 11:26:13 -0500

    proyecto base para el curso de linq.

---
+++ Book.cs
@@ -0,0 +1,9 @@
+public class Book
+{
+    public String Title {get;set;}
+    public int PageCount {get;set;}
+    public String Status {get;set;}
+    public DateTime PublishedDate {get;set;}
+    public String[] Authors {get;set;}
+    public String[] Categories {get;set;}
+}
