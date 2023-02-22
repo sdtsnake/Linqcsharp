927552f524461492a09f7d2accfbd7b5873c793b
Oscar Andres Ramos Lopez <oscar1940_1@hotmail.com>
2023-02-22 08:37:19 -0500

    se realiza la consulta y la carga de los libros

--- Program.cs
+++ Program.cs
@@ -3,10 +3,10 @@ ListAllJson(query.findAll());
 
 void ListAllJson(IEnumerable<Book> ListBooks)
 {
-    Console.WriteLine("{0,-60} {1, 9} {2, 11}\n", "Titulo","N. Pagina","Fecha de publicacion");
+    Console.WriteLine("{0,-60} {1, 15} {2, 15}\n", "Titulo","N. Pagina","Fecha de publicacion");
     foreach(var item in ListBooks)
     {
-        Console.WriteLine("{0,-60} {1, 9} {2, 11}\n",item.Title,item.PageCount,item.PublishedDate);
+        Console.WriteLine("{0,-60} {1, 9} {2, 11}\n",item.Title,item.PageCount,item.PublishedDate.ToShortDateString());
     }
 
 }
